37d9eacffac35634b116efdad0357ca96a00bf47
conejofoz <conejofoz@gmail.comgit config --global user.name conejofozgit config --global user.email conejofoz@gmail.com>
2020-08-18 18:49:44 -0300

    abertura de arquivo com scanner

---
+++ arquivos/src/application/Program.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		File arquivo = new File("C:\\in.txt");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(arquivo);
+			while(sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		} catch (IOException e) {
+			System.out.println("Erro: " + e.getMessage());
+		}
+		finally {
+			if(sc !=null) {
+				sc.close();
+			}
+		}
+		
+
+	}
+
+}
