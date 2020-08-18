1a458fbe4698427212ad15fab72cb8c1f3357724
conejofoz <conejofoz@gmail.comgit config --global user.name conejofozgit config --global user.email conejofoz@gmail.com>
2020-08-18 17:10:59 -0300

    tratamento de exceções forma correta

---
+++ excecoesPersonalizadas/src/model/exceptions/DomainException.java
@@ -0,0 +1,13 @@
+package model.exceptions;
+
+public class DomainException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+	
+	//Criar um contrutor que recebe uma string mensagem como parâmetro e depois repassar esse parâmetro para o contrutor da superclasse
+	//Permite criar uma exceção personalizada passando uma mensagem para ela
+	public DomainException(String msg) {
+		super(msg);
+	}
+
+}
