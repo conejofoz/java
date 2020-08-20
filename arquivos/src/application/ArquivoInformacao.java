b2aac3d26f1f67fede22bf57800c9ef58d9221c8
conejofoz <conejofoz@gmail.com>
2020-08-20 00:33:16 -0300

    informações do arquivo

---
+++ arquivos/src/application/ArquivoInformacao.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class ArquivoInformacao {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a folder path: ");
+		String strPath = sc.nextLine();
+		
+		File path = new File(strPath);
+		
+		System.out.println("Nome do arquivo.......:" + path.getName());
+		System.out.println("Diretório do arquivo..:" + path.getParent());
+		System.out.println("Caminho completo......:" + path.getPath());
+		System.out.println("Caminho absoluto......:" + path.getAbsolutePath());
+
+	}
+
+}
